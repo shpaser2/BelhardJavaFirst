7cd9c05d91967058eb687f755909798034d5416f
Sergey Shpakovsky <shpaser3@yandex.ru>
2019-10-21 22:14:48 +0300

    the conditions are simplified in the fifth task with XOR

--- src/by/belhard/j18/homework2/Task5.java
+++ src/by/belhard/j18/homework2/Task5.java
@@ -9,24 +9,14 @@ public class Task5{
         }
         System.out.println();
         for (int number = 1; number <= 100; number++) {
-            if (number % 5 == 0 && number % 7 != 0
-                    || number % 5 != 0 && number % 7 == 0) {
+            if ((number % 5 == 0) ^ (number % 7 == 0)) {
                 System.out.print(number + " ");
             }
         }
         System.out.println();
         for (int number = 1; number <= 100; number++) {
-            if (number < 10
-            ||  number > 19 && number < 30
-            ||  number > 39 && number < 50
-            ||  number > 59 && number < 70
-            ||  number > 79 && number < 90
-            ||  number > 99)
-            {
-                 continue;
-            }
-            if (number % 5 == 0 && number % 7 != 0
-            ||  number % 5 != 0 && number % 7 == 0)
+            if ((number / 10 % 2 != 0)
+            && ((number % 5 == 0) ^ (number % 7 == 0)))
             {
                 System.out.print(number + " ");
             }
