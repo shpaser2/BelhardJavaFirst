434360eb7ff099c59f3c5f47517118aee3129c33
Sergey Shpakovsky <shpaser3@yandex.ru>
2019-10-26 10:10:34 +0300

    basis for task1 homework5

---
+++ src/by/belhard/j18/homework5/task1/TableMaterial.java
@@ -0,0 +1,5 @@
+package by.belhard.j18.homework5.task1;
+
+public enum TableMaterial {
+    WOOD, STEEL, GLASS, PLASTIC
+}
---
+++ src/by/belhard/j18/homework5/task1/TableShape.java
@@ -0,0 +1,5 @@
+package by.belhard.j18.homework5.task1;
+
+public enum TableShape {
+    ROUND, TRAPEZOIDAL, RECTANGULAR, SQUARE, TRIANGULAR, OTHER
+}
